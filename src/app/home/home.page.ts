2a763f4aa359f0a5eb462a7d5c1d20bb5c84000d
王星 <675046199@qq.com>
2018-08-13 10:11:15 +0800

    体验back-btn组件，导航‘返回’按钮

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,8 +1,23 @@
 import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss']
 })
-export class HomePage {}
+export class HomePage {
+
+  constructor( private navController:NavController){
+
+  }
+
+  /**
+   * 打开一个新的页面
+   */
+  goNewPage():void{
+      // this.navController.goForward('/tabs/(contact:contact)',true);
+
+      this.navController.goForward('/test',true);
+  }
+}
