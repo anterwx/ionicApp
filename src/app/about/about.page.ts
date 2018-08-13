fc209314feec75143e5e7b73988af5121d0a7273
疯子 <anterwx@users.noreply.github.com>
2018-08-13 14:41:43 +0800

    体验slides，目前beta版本还存在BUG

--- src/app/about/about.page.ts
+++ src/app/about/about.page.ts
@@ -1,8 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { Slides, Slide } from '@ionic/angular';
 
 @Component({
   selector: 'app-about',
   templateUrl: 'about.page.html',
   styleUrls: ['about.page.scss']
 })
-export class AboutPage {}
+export class AboutPage {
+
+  @ViewChild(Slides) slides: Slides;
+
+  //slides的配置项
+  slideOpts:any = {
+    autoplay:true
+  };
+
+  //滑动到第二个滑块
+  goToSlide() {
+    this.slides.slideTo(1, 1000);
+  }
+
+  //监听滑块变化事件
+  slideChanged(){
+    let currentSlideIndex = this.slides.getActiveIndex();
+    console.log('The current slide Index is ' + currentSlideIndex);
+  }
+  ngAfterViewInit(){
+    console.log(this.slides);
+  }
+}
