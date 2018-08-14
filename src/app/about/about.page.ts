b53cbfe16cc3c6518ac561fc3b0dc9d1fde395d5
疯子 <anterwx@users.noreply.github.com>
2018-08-14 10:33:11 +0800

    实践slide组件的用法

--- src/app/about/about.page.ts
+++ src/app/about/about.page.ts
@@ -12,19 +12,40 @@ export class AboutPage {
 
   //slides的配置项
   slideOpts:any = {
-    autoplay:true
+    autoplay:true,
+    slidesPerView: 'auto',
+    centeredSlides:true,
+    spaceBetween:15
   };
 
   //滑动到第二个滑块
-  goToSlide() {
-    this.slides.slideTo(1, 1000);
+  goToSlide(index) {
+    this.slides.slideTo(index, 1000);
+    console.log('swipe '+index+' is active.');
   }
-
+  //swipe初始化完成后回调
+  slideLoaded(){
+    console.log('Swipe initialization.')
+    this.slides.update();
+  }
   //监听滑块变化事件
   slideChanged(){
     let currentSlideIndex = this.slides.getActiveIndex();
-    console.log('The current slide Index is ' + currentSlideIndex);
+    console.log('The current slide Index is ' , currentSlideIndex);
   }
+  //当点击滑块时
+  slideTap(){
+    
+  }
+  //当触摸开始时
+  slideTouchStart(){
+    this.slides.stopAutoplay();
+  }
+  //当触摸结束后
+  slideTouchEnd(){
+    this.slides.startAutoplay();
+  }
+  //页面渲染完后
   ngAfterViewInit(){
     console.log(this.slides);
   }
